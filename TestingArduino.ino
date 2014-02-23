e3593ba2f0e2a6064718a7e9b049d88f4d8d5e83
John McCall <mcca0798@morris.umn.edu>
2014-02-23 14:25:04 -0600

    Adding the test file Andrew created to try out using motors with the NXShield

---
+++ TestingArduino.ino
@@ -0,0 +1,51 @@
+//Motor1 Bank A
+#include <Wire.h>
+#include <NXShield.h>
+
+NXShield nxshield;
+
+void
+setup(){
+  Serial.begin(115200);
+  delay(2000);
+  long rotations = 10;
+  char str[40];
+  
+  nxshield.init(SH_HardwareI2C);
+  
+  //waiting for go b
+  nxshield.waitForButtonPress(BTN_GO);
+  
+  nxshield.bank_a.motorReset();
+  
+  delay(1000);
+  str[0] = '\0';
+  nxshield.bank_a.motorStartBothInSync();
+  nxshield.bank_a.motorRunRotations(SH_Motor_1,
+                  SH_Direction_Forward,
+                  100,
+                  rotations,
+                  SH_Completion_Wait_For,
+                  SH_Next_Action_BrakeHold);
+  nxshield.bank_a.motorRunRotations(SH_Motor_1,
+                  SH_Direction_Reverse,
+                  100,
+                  rotations,
+                  SH_Completion_Wait_For,
+                  SH_Next_Action_BrakeHold);
+//  nxshield.bank_b.motorRunRotations(SH_Motor_Both,
+//                  SH_Direction_Forward,
+//                  100,
+//                  rotations,
+//                  SH_Completion_Wait_For,
+//                  SH_Next_Action_BrakeHold);
+    
+}
+void loop(){
+  nxshield.ledSetRGB(8,0,0);
+  delay(1000);
+  nxshield.ledSetRGB(0,0,0);
+  delay(1000);
+  
+}
+#
