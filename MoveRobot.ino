01afc14e890205dcbe79c75e006e247a8d59d598
John McCall <mcca0798@morris.umn.edu>
2014-03-02 17:09:26 -0600

    Added the code for moving the robot and for using the US sensor.

---
+++ MoveRobot.ino
@@ -0,0 +1,93 @@
+//Motor1 Bank A
+#include <Wire.h>
+#include <NXShield.h>
+//Don't make # symbols
+//Bank_A: front-right:1, back-right:2
+//Bank_B: front-left: 1, back-left: 2
+
+
+NXShield nxshield;
+
+void
+setup(){
+  Serial.begin(115200);
+  delay(2000);
+  char str[40];
+  
+  nxshield.init(SH_HardwareI2C);
+  
+  nxshield.waitForButtonPress(BTN_GO);
+  
+  nxshield.bank_a.motorReset();
+  nxshield.bank_b.motorReset();
+}
+
+void
+loop(){
+  nxshield.ledSetRGB(8,0,0);
+  delay(1000);
+  nxshield.ledSetRGB(0,8,0);
+  delay(1000);
+  nxshield.ledSetRGB(0,0,8);
+  delay(1000);
+  nxshield.ledSetRGB(0,0,0);
+  delay(1000);
+}
+void
+moveForward(int speed1){
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, speed1);
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Reverse, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Reverse, speed1);
+}
+void
+moveBackward(int speed1){
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, speed1);
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, speed1);
+}
+void
+stopMoving(){
+  nxshield.bank_a.motorStop(SH_Motor_Both, SH_Next_Action_Float);
+  nxshield.bank_b.motorStop(SH_Motor_Both, SH_Next_Action_Float);
+}
+void
+strafeLeft(int speed1){
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, speed1);
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Reverse, speed1);
+}
+void
+strafeRight(int speed1){
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, speed1);
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Reverse, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, speed1);
+  
+}
+void
+turnClockwise(int speed1){
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, speed1);
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Reverse, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, speed1);
+}
+void
+turnCounterClockwise(int speed1){
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, speed1);
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, speed1);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Reverse, speed1);
+}
+
+void
+diagonal(){
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_1, SH_Direction_Reverse, 40);
+  nxshield.bank_b.motorRunUnlimited(SH_Motor_2, SH_Direction_Reverse, 90);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_1, SH_Direction_Forward, 90);
+  nxshield.bank_a.motorRunUnlimited(SH_Motor_2, SH_Direction_Forward, 40);
+  
+}
+#
---
+++ NXTUSTest.ino
@@ -0,0 +1,61 @@
+#include <Wire.h>
+#include <NXShield.h>
+#include <NXTUS.h>
+
+// setup for this example:
+// attach external power to NXShield.
+// attach LEGO Ultrasonic sensors to BAS2 and BBS2
+// Open the Serial terminal to view.
+
+
+//
+// declare the NXShield(s) attached to your Arduino.
+//
+NXShield    nxshield;
+
+//
+// declare the i2c devices used on NXShield(s).
+//
+NXTUS       sonar1;
+NXTUS       sonar2;
+
+void setup() {
+
+  Serial.begin(115200);  // start serial for output
+  delay(500); // wait, allowing time to activate the serial monitor
+
+  Serial.println (__FILE__);
+  Serial.println ("Initializing the devices ...");
+  //
+  // Initialize the protocol for NXShield
+  // It is best to use Hardware I2C (unless you want to use Ultrasonic).
+  //
+  nxshield.init( SH_SoftwareI2C );
+
+  //
+  // Wait until user presses GO button to continue the program
+  //
+  Serial.println ("Press GO button to continue");
+  nxshield.waitForButtonPress(BTN_GO);
+
+  //
+  // Initialize the i2c sensors.
+  //
+  sonar1.init( &nxshield, SH_BBS1 );
+  sonar2.init( &nxshield, SH_BAS1 );
+}
+  
+void loop() {
+ 
+ char str1[256];
+ //char str2[256];
+ 
+ sprintf (str1, "sonar1: %d", sonar1.getDist());
+ //sprintf (str2, "sonar2: %d", sonar2.getDist());
+ 
+ Serial.println(str1);
+ delay(1000);
+ //Serial.println(str2);
+ 
+}
+  
